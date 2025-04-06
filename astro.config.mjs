c6c3ac7793a578c741e0beb0372bdffcd674dd84
Kevin Dang <dangkevin12@gmail.com>
2025-04-05 21:21:58 -0400

    remove base param

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://k-dang.github.io/',
-  // base: '/personal-site',
 
   vite: {
     plugins: [tailwindcss()],
